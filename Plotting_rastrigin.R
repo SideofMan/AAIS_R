11d3d3c7e072730c62cfd33ea1b9889699a96308
SideofMan <sideofman@gmail.com>
2024-01-17 16:25:37 -0600

    Rastrigin plots

--- Plotting_rastrigin.R
+++ Plotting_rastrigin.R
@@ -9,7 +9,7 @@ plot_ly()%>%
   add_surface(x = x, y = y, z = matrix(Z, nrow = length(x), ncol = length(y), byrow = TRUE), alpha = 1, colorscale = 'Rainbow') %>%
   # layout(title = '2D Rastrigin function') %>%
   layout(scene = list(aspectmode = "manual", aspectratio = list(x=1,y=1,z=0.5),
-                      xaxis = list(range = c(-5.12,5.12)), 
-                      yaxis = list(range = c(-5.12,5.12)),
-                      zaxis = list(range = c(0,80))))%>%
+                      xaxis = list(titlefont = list(size = 25), tickfont = list(size = 15), range = c(-5.12,5.12)), 
+                      yaxis = list(titlefont = list(size = 25), tickfont = list(size = 15), range = c(-5.12,5.12)),
+                      zaxis = list(titlefont = list(size = 25), tickfont = list(size = 15), dtick = 20, range = c(0,80))))%>%
   hide_colorbar()
